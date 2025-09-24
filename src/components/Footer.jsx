ab37986c397fd68faf63c3ee67b8c8b94da4fca2
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-24 17:33:12 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+     return (<footer> <h3>sono il footer</h3> </footer>); 
+    }
---
+++ src/components/Main.jsx
@@ -0,0 +1,3 @@
+export default function Main() { 
+    return (<main> <h2>sono il main</h2> </main>); 
+}
