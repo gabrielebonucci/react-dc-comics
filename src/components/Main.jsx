d0ab93db1ba17fc880731c5a762b15d19945a45b
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-24 18:23:49 +0200

    base html main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import digComics from '../assets/img/buy-comics-digital-comics.png';
 import merchandise from '../assets/img/buy-comics-merchandise.png';
+import subscription from '../assets/img/buy-comics-subscriptions.png';
 import locationShop from '../assets/img/buy-comics-shop-locator.png';
 import power from '../assets/img/buy-dc-power-visa.svg';
 
@@ -11,10 +12,11 @@ export default function Main() {
         </div>
         <div>
             <ul>
-                <li> <img src={digComics} alt="Digital comics" /> </li>
-                <li> <img src={merchandise} alt="Digital comics" /> </li>
-                <li> <img src={locationShop} alt="Digital comics" /></li>
-                <li> <img src={power} alt="Digital comics" /></li>
+                <li> <span>DIGITAL COMICS</span> <img src={digComics} alt="Digital comics" /> </li>
+                <li> <span>DC MERCHANDISE</span>   <img src={merchandise} alt="merchandise" /> </li>
+                <li> <span>SUBSCRITION</span>  <img src={subscription} alt="subscription" /> </li>
+                <li> <span>COMIC SHOP LOCATOR</span> <img src={locationShop} alt="locationShop" /></li>
+                <li> <span>DC POWER VISA</span> <img src={power} alt="power" /></li>
             </ul>
         </div>
     </main>
